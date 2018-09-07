f2d8e211c1b3cecb6046ea82006883ea466caa5c
lubalis <lubinska.alicja@gmail.com>
2018-09-07 10:50:15 +0200

    webpack config added

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+    mode: 'development',
+    entry: "./js/app.js",
+    output: {  
+      path: path.join(__dirname, "./js/"), 
+      filename : 'out.js' 
+    },
+    watch: true,
+    module: {
+        rules : [{
+            test: /\.jsx$/,  exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: [["env", {
+                targets: {
+                  browsers: ['> 1%']
+                }
+              }]]
+            }
+        }]
+    }
+};
